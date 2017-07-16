6536c48fad3d914d307b9fcff5e2bd53dab371c0
decksterr <arthurpicerna@gmail.com>
2017-07-16 07:00:54 -0500

    test

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -4,7 +4,7 @@
 
   <div class="section">
     <div class="row center">
-      <h3 class="light header">El conflicto armado en Colombia</h3>
+      <h3 class="light header">Test</h3>
       <p class="col s12 m8 offset-m2 caption">
         El conflicto armado interno en Colombia es una guerra asimétrica que se desarrolla en Colombia desde la década de 1960 hasta la actualidad.24​ Los principales actores involucrados han sido en un comienzo el Estado colombiano y las guerrillas de extrema
         izquierda, sumándose décadas después los grupos paramilitares de extrema derecha, los carteles del narcotráfico y las bandas criminales. Ha pasado por varias etapas de recrudecimiento, en especial desde los años ochenta cuando algunos de los actores
